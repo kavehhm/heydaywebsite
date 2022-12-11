cc36bdb1556fddccff1854cefac4870143884af4
Kaveh Malekzadeh <kavehm013@gmail.com>
2022-12-10 20:32:20 -0800

    ain

--- src/Components/Calendar-Section.js
+++ src/Components/Calendar-Section.js
@@ -7,7 +7,7 @@ const Calendar = forwardRef((props, ref) => {
   // const auth = 'eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjY5NzkyNzY3LCJqdGkiOiJlMGIzOGJiNi0zY2M4LTQ2YmEtYjY1ZC04ZDMwMDNlZDQ5MDYiLCJ1c2VyX3V1aWQiOiIyMGYzODBlMC1kMjNjLTQ3NmItYTFjZi1lMDM2ZWQ2NTY3YzkifQ.a4lAUBax1nPafqfJQGA_QeLbVKRrmH54BUbr5CFTOTrs1XR0Q0QUKmQ9tzsMHzkataBOsfCFm-uboAu-lO7MuQ'
   return (
     <div className="appointment">
-      <div className="widget" ref={ref} >
+      <div className="widget" ref={ref}>
         <InlineWidget
           styles={{
             height: "50rem",
@@ -21,15 +21,16 @@ const Calendar = forwardRef((props, ref) => {
         <h1 className="free-demo">Free 15-Minute Demo Call</h1>
         <p className="text-block">
           By the end of this Audit call, you will have a clear understanding of
-          the next steps you can take for your business to start generating
-          consistent and reliable results online with Funnels & Paid
-          Advertising.
+          the next steps you can take for your business to refresh its online
+          presence.
         </p>
         <p className="text-block">
           Find a time on Kaveh’s calendar to schedule your call today and we
           look forward to speaking to you soon!
         </p>
-        <h1 className="perfect-for"><b>THIS AUDIT CALL IS PERFECT FOR:</b></h1>
+        <h1 className="perfect-for">
+          <b>THIS AUDIT CALL IS PERFECT FOR:</b>
+        </h1>
         <ul>
           <li>
             <img src={Check} alt="Check" />
@@ -47,8 +48,8 @@ const Calendar = forwardRef((props, ref) => {
           <li>
             <img src={Check} alt="Check" />
             <p>
-              ​Businesses looking to understand their{" "}
-              <b>increased revenue potential </b> with funnels & conversion rate
+              ​Businesses looking to increase their{" "}
+              <b> revenue potential </b> with new design & search engine
               optimization.
             </p>
           </li>
@@ -56,7 +57,7 @@ const Calendar = forwardRef((props, ref) => {
             <img src={Check} alt="Check" />
             <p>
               Businesses looking to{" "}
-              <b>maximize their conversion rates & average order value.</b>
+              <b>maximize their online conversion rates & eyeballs reached.</b>
             </p>
           </li>
           <li>
